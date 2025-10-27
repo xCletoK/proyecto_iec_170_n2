f6a72b412bcab18cd1076afbd81917fe9212aaf7
Gesgg <essus_94@live.cl>
2025-10-27 17:49:14 -0300

    Refactor Vuelo class to inherit from Base

--- modelos/vuelo.py
+++ modelos/vuelo.py
@@ -1,14 +1,9 @@
 from sqlalchemy import Column, Integer, String, Date, Time
-from sqlalchemy.ext.declarative import declarative_base
+from modelos.base import Base
 
-base = declarative_base()
-
-
-
-
-class Vuelo(base):
+class Vuelo(Base):
     __tablename__ = 'vuelo'
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     origen = Column(String(10), nullable=False)
     destino = Column(String(10), nullable=False)
@@ -17,13 +12,3 @@ class Vuelo(base):
     fecha_llegada = Column(Date, nullable=False)
     hora_llegada = Column(Time, nullable=False)
     cantidad_asientos = Column(Integer, nullable=False)
-
-class Vuelo():
-    def __init__(self, origen, destino, fecha_salida, hora_salida, fecha_llegada, hora_llegada, cantidad_asientos):
-        self.origen = origen
-        self.destino = destino
-        self.fecha_salida = fecha_salida
-        self.hora_salida = hora_salida
-        self.fecha_llegada = fecha_llegada
-        self.hora_llegada = hora_llegada
-        self.cantidad_asientos = cantidad_asientos
