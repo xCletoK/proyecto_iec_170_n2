46aecfef6e186df60490cd3a8247909556627240
Gesgg <essus_94@live.cl>
2025-10-27 17:48:08 -0300

    Fix database connection string format
    Updated DATABASE_URL to include port number.

--- datos/conexion.py
+++ datos/conexion.py
@@ -1,13 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# pip install sqalchemy
+# pip install sqlalchemy
 # pip install mysql-connector-python
 
-# definir cadena de conexión
-# mysql+mysqlconnector://usuario:contraseña@host:puerto/nombre_base_datos
-
-DATABASE_URL = "mysql+mysqlconnector://root:root@localhost/iec_170_n2"
-
+DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/iec_170_n2"
 motor_db = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=motor_db)
