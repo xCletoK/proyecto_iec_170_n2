4ad917b01c5e6461bbfc626e09de74324d2af6e6
Gesgg <essus_94@live.cl>
2025-10-27 17:50:02 -0300

    Fix import statements in boleto.py

--- modelos/boleto.py
+++ modelos/boleto.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey
-from datos.conexion import Base
+ffrom sqlalchemy import Column, Integer, String, Date, DECIMAL, ForeignKey
+from modelos.base import Base
 
 class Boleto(Base):
     __tablename__ = 'boleto'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     numero_asiento = Column(Integer, nullable=False)
     fecha_compra = Column(Date, nullable=False)
     tarifa = Column(DECIMAL(10, 2), nullable=False)
-
-    cod_vuelo = Column(Integer, ForeignKey('vuelo.id'), nullable=False)
-    rut_pasajero = Column(String(10), ForeignKey('pasajero.rut'), nullable=False)
+    cod_vuelo = Column(Integer, ForeignKey("vuelo.id"), nullable=False)
+    rut_pasajero = Column(String(10), ForeignKey("pasajero.rut"), nullable=False)
