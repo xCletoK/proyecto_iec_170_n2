a12c52face2e26ece0e171551515ca5f70243da5
Matías Eduardo Díaz <114715156+xCletoK@users.noreply.github.com>
2025-11-05 16:34:29 -0300

    changes_05.11.2025_16.34

--- inicio.py
+++ inicio.py
@@ -1,6 +1,3 @@
-from datos.conexion import Session
-from datos.obtener_datos import obtener_datos_objetos, obtener_datos_boleto
-from interfaz_usuario.iu_boletos import ingresar_datos_boleto
-from prettytable import PrettyTable
-from modelos.boleto import Boleto
+from iu.menu_principal import menu_principal
 
+menu_principal()
